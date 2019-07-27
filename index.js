2b9d7578d673166f658317193821fc11554251c4
Nawaz Dhandala <nawazdhandala@outlook.com>
2019-07-27 19:22:01 +0530

    push changes.

--- index.js
+++ index.js
@@ -114,5 +114,28 @@ async function run() {
 	}
 }
 
+process.stdin.resume();  //so the program will not close instantly
+
+async function exitHandler(options, exitCode) {
+	console.log("Stopping everything. ")
+	await stopEverything();
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
 
 run();

bd18b9cfad1fc8c8657e63d96cf0994b27b478f4
Nawaz Dhandala <nawazdhandala@outlook.com>
2019-07-28 19:57:25 +0530

    push changes.

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 var gpio = require('rpi-gpio')
-var sleep = require('sleep').sleep;
-var msleep = require('sleep').msleep;
+var sleep = require('await-sleep');
 var gpiop = gpio.promise;
 
 async function run() {
 
-	//LED
-	await gpiop.setup(7, gpio.DIR_OUT)
-	await gpiop.setup(11, gpio.DIR_OUT)
+	function setup() {
+		//LED
+		await gpiop.setup(7, gpio.DIR_OUT)
+		await gpiop.setup(11, gpio.DIR_OUT)
 
-	//Motors 1
-	await gpiop.setup(40, gpio.DIR_OUT)
-	await gpiop.setup(29, gpio.DIR_OUT)
-	await gpiop.setup(33, gpio.DIR_OUT)
-	await gpiop.setup(32, gpio.DIR_OUT)
+		//Motors 1
+		await gpiop.setup(40, gpio.DIR_OUT)
+		await gpiop.setup(29, gpio.DIR_OUT)
+		await gpiop.setup(33, gpio.DIR_OUT)
+		await gpiop.setup(32, gpio.DIR_OUT)
 
-	//Motors 2
-	await gpiop.setup(37, gpio.DIR_OUT)
-	await gpiop.setup(19, gpio.DIR_OUT)
-	await gpiop.setup(21, gpio.DIR_OUT)
-	await gpiop.setup(13, gpio.DIR_OUT)
+		//Motors 2
+		await gpiop.setup(37, gpio.DIR_OUT)
+		await gpiop.setup(19, gpio.DIR_OUT)
+		await gpiop.setup(21, gpio.DIR_OUT)
+		await gpiop.setup(13, gpio.DIR_OUT)
+	}
 
-	async function stop(){
+
+	async function stop() {
+		console.log("Stopping all ports");
 		await gpiop.write(40, false)
 		await gpiop.write(29, false)
 		await gpiop.write(33, false)
@@ -38,113 +41,132 @@ async function run() {
 		await gpiop.write(11, false)
 	}
 
-	//clear all the ports first. 
-	await stop();
+	
 
 
-	async function test(){
+	async function test() {
 		//LED Test
 		console.log("Led Test");
 		console.log("Led Test 1");
 		console.log("Turning on")
 		await gpiop.write(7, true)
-		msleep(100);
+		await sleep(100);
 		console.log("Turning off")
 		await gpiop.write(7, false)
-		msleep(100)
+		await sleep(100)
 		console.log("Turning on")
 		await gpiop.write(7, true)
-		msleep(100);
+		await sleep(100);
 		console.log("Turning off")
 		await gpiop.write(7, false)
-		sleep(1)
+		await sleep(1000)
 
 		console.log("Led Test 2");
 		console.log("Turning on")
 		await gpiop.write(11, true)
-		msleep(100);
+		await sleep(100);
 		console.log("Turning off")
 		await gpiop.write(11, false)
-		msleep(100)
+		await sleep(100)
 		console.log("Turning on")
 		await gpiop.write(11, true)
-		msleep(100);
+		await sleep(100);
 		console.log("Turning off")
 		await gpiop.write(11, false)
-		sleep(1)
+		await sleep(1000)
 
-		console.log("Starting A Motor");
-		sleep(2)
+		console.log("Motor Test");
+		console.log("Port 40 on");
+		await sleep(2000)
 		await stop();
 		await gpiop.write(40, true)
 
-		console.log("Starting A Motor");
-		sleep(2)
+		console.log("Port 29 on");
+		await sleep(2000)
 		await stop();
 		await gpiop.write(29, true)
 
-		console.log("Starting A Motor");
-		sleep(2)
+		console.log("Port 33 on");
+		await sleep(2000)
 		await stop();
 		await gpiop.write(33, true)
 
-		console.log("Starting A Motor");
-		sleep(2)
+		console.log("Port 32 on");
+		await sleep(2000)
 		await stop();
 		await gpiop.write(32, true)
 
 		//Motors 2
 
-		console.log("Starting A Motor");
-		sleep(2)
+		console.log("Port 37 on");
+		await sleep(2000)
 		await stop();
 		await gpiop.write(37, true)
 
-		console.log("Starting A Motor");
-		sleep(2)
+		console.log("Port 19 on");
+		await sleep(2000)
 		await stop();
 		await gpiop.write(19, true)
 
-		console.log("Starting A Motor");
-		sleep(2)
+		console.log("Port 21 on");
+		await sleep(2000)
 		await stop();
 		await gpiop.write(21, true)
 
-		console.log("Starting A Motor");
-		sleep(2)
+		console.log("Port 13 on");
+		await sleep(2000)
 		await stop();
 		await gpiop.write(13, true)
-		console.log("Starting A Motor");
-		sleep(2)
+		await sleep(2000)
+
+		console.log("Stopping test");
 		await stop();
 	}
 
-	await test();
+	async function motorOn() {
+
+	}
+
+	async function ledOn() {
+		while (true) {
+			console.log("Turning on")
+			await gpiop.write(7, true)
+			await sleep(100);
+			console.log("Turning off")
+			await gpiop.write(7, false)
+			await sleep(100)
+			console.log("Turning on")
+			await gpiop.write(7, true)
+			await sleep(100);
+			console.log("Turning off")
+			await gpiop.write(7, false)
+			await sleep(1000)
+
+			console.log("Led Test 2");
+			console.log("Turning on")
+			await gpiop.write(11, true)
+			await sleep(100);
+			console.log("Turning off")
+			await gpiop.write(11, false)
+			await sleep(100)
+			console.log("Turning on")
+			await gpiop.write(11, true)
+			await sleep(100);
+			console.log("Turning off")
+			await gpiop.write(11, false)
+			await sleep(1000)
+		}
+	}
+
+	await setup();
+	//stop and clear all ports on startup. 
 	await stop();
+	//test 
+	await test();
+	//stop and clear all ports. 
+	await stop();
+
+	ledOn();
 }
-
-// process.stdin.resume();  //so the program will not close instantly
-
-// async function exitHandler(options, exitCode) {
-// 	console.log("Stopping everything. ")
-// 	await stop();
-//     if (options.cleanup) console.log('clean');
-//     if (exitCode || exitCode === 0) console.log(exitCode);
-//     if (options.exit) process.exit();
-// }
-
-// //do something when app is closing
-// process.on('exit', exitHandler.bind(null, {cleanup:true}));
-
-// //catches ctrl+c event
-// process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-// // catches "kill pid" (for example: nodemon restart)
-// process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-// process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
-
-// //catches uncaught exceptions
-// process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-
 
 run();
